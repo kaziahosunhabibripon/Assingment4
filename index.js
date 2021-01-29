e261e80845bdb15c22e86cc0117986b5e640d708
Ripon <habibkazi92@gmail.com>
2021-01-30 06:38:31 +0800

    transaction add successfully

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
-function ticketChangeHandler(air, isIncrease){
+// transaction part
+
+const orderBtn = document.getElementById("oder");
+orderBtn.addEventListener("click", function () {
+    const bookingForm = document.getElementById("bookingPart");
+    bookingForm.style.display = "none";
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+
+})
+
+function ticketChangeHandler(air, isIncrease) {
     const airTicketInputCount = getTicketValue(air);
     let airTicketNewInputCount = airTicketInputCount;
-    if ( isIncrease == true){
+    if (isIncrease == true) {
         airTicketNewInputCount = (airTicketInputCount + 1);
     }
-    if ( isIncrease == false && airTicketInputCount > 0){
+    if (isIncrease == false && airTicketInputCount > 0) {
         airTicketNewInputCount = (airTicketInputCount - 1);
     }
 
     document.getElementById(air + "TicketCount").value = airTicketNewInputCount;
 
     let airTTicketPrice = 0;
-    if(air == 'firstClass'){
-        airTicketPrice = airTicketNewInputCount * 150 ;
-        document.getElementById(air + "TicketPrice").innerText = "First Class($" + airTicketPrice +")";
+    if (air == 'firstClass') {
+        airTicketPrice = airTicketNewInputCount * 150;
+        document.getElementById(air + "TicketPrice").innerText = "First Class($" + airTicketPrice + ")";
     }
-    if(air == 'economy'){
-        airTicketPrice = airTicketNewInputCount * 100 ;
-        document.getElementById(air + "TicketPrice").innerText = "Economy ($" + airTicketPrice +")";
-    }  
+    if (air == 'economy') {
+        airTicketPrice = airTicketNewInputCount * 100;
+        document.getElementById(air + "TicketPrice").innerText = "Economy ($" + airTicketPrice + ")";
+    }
     totalPriceCalculation();
 }
 
-function totalPriceCalculation(){
+function totalPriceCalculation() {
     const firstClassTicketCount = getTicketValue('firstClass');
     const economyTicketCount = getTicketValue('economy');
-    const totalPrice = economyTicketCount * 100 + firstClassTicketCount * 150; 
+    const totalPrice = economyTicketCount * 100 + firstClassTicketCount * 150;
     document.getElementById("subTotal").innerText = "$" + totalPrice;
     const taxAmount = Math.round(totalPrice * .1);
-        document.getElementById("tax").innerText = "$" + taxAmount;
-    const grandTotal  = (totalPrice + taxAmount);   
-        document.getElementById("total").innerText = "$" + grandTotal;
+    document.getElementById("tax").innerText = "$" + taxAmount;
+    const grandTotal = (totalPrice + taxAmount);
+    document.getElementById("total").innerText = "$" + grandTotal;
 }
 
-function getTicketValue(air){
+function getTicketValue(air) {
     const airTicketInput = document.getElementById(air + "TicketCount");
     const airTicketCount = parseInt(airTicketInput.value);
     return airTicketCount;
 }
+
+
+const display = document.querySelector("transaction-area");
+const displayOutput = document.querySelector(".booking-form");
+displayOutput.addEventListener("click", function() {
+    const totalAmount = total.value;
+
+})
+
+
+
