2b5f43ca5239e726d38943ddb03d401759f19876
Ripon <habibkazi92@gmail.com>
2021-01-30 04:10:36 +0800

    total price add successfully

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function ticketChangeHandler(air, isIncrease){
-    const airTicketInput = document.getElementById(air + "TicketCount");
-    const airTicketInputCount = parseInt(airTicketInput.value);
+    const airTicketInputCount = getTicketValue(air);
     let airTicketNewInputCount = airTicketInputCount;
     if ( isIncrease == true){
         airTicketNewInputCount = (airTicketInputCount + 1);
@@ -9,7 +8,7 @@ function ticketChangeHandler(air, isIncrease){
         airTicketNewInputCount = (airTicketInputCount - 1);
     }
 
-    airTicketInput.value = airTicketNewInputCount;
+    document.getElementById(air + "TicketCount").value = airTicketNewInputCount;
 
     let airTTicketPrice = 0;
     if(air == 'firstClass'){
@@ -24,13 +23,18 @@ function ticketChangeHandler(air, isIncrease){
 }
 
 function totalPriceCalculation(){
-    const economyTicketInput = document.getElementById("economyTicketCount");
-    const economyTicketCount = parseInt(economyTicketInput.value);
-
-
-    const firstClassTicketInput = document.getElementById("firstClassTicketCount");
-    const firstClassTicketCount = parseInt(firstClassTicketInput.value);
-
+    const firstClassTicketCount = getTicketValue('firstClass');
+    const economyTicketCount = getTicketValue('economy');
     const totalPrice = economyTicketCount * 100 + firstClassTicketCount * 150; 
     document.getElementById("subTotal").innerText = "$" + totalPrice;
+    const taxAmount = Math.round(totalPrice * .1);
+        document.getElementById("tax").innerText = "$" + taxAmount;
+    const grandTotal  = (totalPrice + taxAmount);   
+        document.getElementById("total").innerText = "$" + grandTotal;
+}
+
+function getTicketValue(air){
+    const airTicketInput = document.getElementById(air + "TicketCount");
+    const airTicketCount = parseInt(airTicketInput.value);
+    return airTicketCount;
 }
